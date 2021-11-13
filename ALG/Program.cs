9962f2a5f4dcc89258838c718bcef923823e6bf7
yorek <info@davidemauri.it>
2021-11-12 18:08:03 -0800

    switched to .net 5.0 to be compatible with GitHub Actions

--- ALG/Program.cs
+++ ALG/Program.cs
@@ -1,95 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 using YamlDotNet.RepresentationModel;
 using DotLiquid;
 
-namespace AwesomeList;
-
-public class AwesomeItem
+namespace AwesomeList
 {
-    public string Title { get; init; } = string.Empty;
-    public string Description { get; init; } = string.Empty;
-    public string Url { get; init; } = string.Empty; 
-    public string Icon { get; init; } = string.Empty; 
-}
 
-internal class Program
-{
-    static string yamlFile = "../items.yml";
-    
-    static string liquidTemplate = "../readme.liquid";
-
-    static string outputFile = "../README.md";
-
-    static Dictionary<string, List<AwesomeItem>> tagList = new ();
-
-    static void Main(string[] args)
+    public class AwesomeItem
     {
-        Console.WriteLine($"Processing YAML file ({yamlFile})...");
+        public string Title { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public string Url { get; init; } = string.Empty;
+        public string Icon { get; init; } = string.Empty;
+    }
 
-        using (StreamReader reader = File.OpenText(yamlFile))
+    public class Program
+    {
+        static string yamlFile = "../items.yml";
+
+        static string liquidTemplate = "../readme.liquid";
+
+        static string outputFile = "../README.md";
+
+        static Dictionary<string, List<AwesomeItem>> tagList = new();
+
+        static void Main(string[] args)
         {
-            var yaml = new YamlStream();
-            yaml.Load(reader);
+            Console.WriteLine($"Processing YAML file ({yamlFile})...");
 
-            var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-
-            foreach (var entry in mapping.Children)
+            using (StreamReader reader = File.OpenText(yamlFile))
             {
-                var node = ((YamlMappingNode)entry.Value).Children;
+                var yaml = new YamlStream();
+                yaml.Load(reader);
 
-                var title = ((YamlScalarNode)entry.Key).Value ?? string.Empty;
-                var description = ((YamlScalarNode)node["description"]).Value ?? string.Empty;
-                var url = ((YamlScalarNode)node["url"]).Value ?? string.Empty;
-                var icon = node.ContainsKey("icon") ? ((YamlScalarNode)node["icon"]).Value ?? string.Empty : string.Empty;
+                var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
 
-                var item = new AwesomeItem() { Title = title, Description = description, Url = url, Icon = icon } ;                
-
-                var tags = ((YamlSequenceNode)node["tags"]).Children;
-                foreach(var t in tags)
+                foreach (var entry in mapping.Children)
                 {
-                    string tag = ((YamlScalarNode)t).Value ?? string.Empty;
-                    if (!tagList.ContainsKey(tag))
-                        tagList.Add(tag, new List<AwesomeItem>());
+                    var node = ((YamlMappingNode)entry.Value).Children;
 
-                    tagList[tag].Add(item);
+                    var title = ((YamlScalarNode)entry.Key).Value ?? string.Empty;
+                    var description = ((YamlScalarNode)node["description"]).Value ?? string.Empty;
+                    var url = ((YamlScalarNode)node["url"]).Value ?? string.Empty;
+                    var icon = node.ContainsKey("icon") ? ((YamlScalarNode)node["icon"]).Value ?? string.Empty : string.Empty;
+
+                    var item = new AwesomeItem() { Title = title, Description = description, Url = url, Icon = icon };
+
+                    var tags = ((YamlSequenceNode)node["tags"]).Children;
+                    foreach (var t in tags)
+                    {
+                        string tag = ((YamlScalarNode)t).Value ?? string.Empty;
+                        if (!tagList.ContainsKey(tag))
+                            tagList.Add(tag, new List<AwesomeItem>());
+
+                        tagList[tag].Add(item);
+                    }
                 }
             }
-        }
 
-        Console.WriteLine($"Processing Liquid template ({liquidTemplate})...");
-        
-        var liquid = File.ReadAllText(liquidTemplate);
-        Template template = Template.Parse(liquid);        
+            Console.WriteLine($"Processing Liquid template ({liquidTemplate})...");
 
-        var liquidTagList = new Dictionary<string, object>();
-        foreach(var t in tagList.OrderBy(i => i.Key)) {               
-            var ail = new List<Object>();
+            var liquid = File.ReadAllText(liquidTemplate);
+            Template template = Template.Parse(liquid);
 
-            foreach(var ai in t.Value)
+            var liquidTagList = new Dictionary<string, object>();
+            foreach (var t in tagList.OrderBy(i => i.Key))
             {
-                var liquidItem = new {
-                    Title = ai.Title,
-                    Description = ai.Description,
-                    Url = ai.Url,
-                    Icon = ai.Icon
-                };
+                var ail = new List<Object>();
 
-                ail.Add(liquidItem);
+                foreach (var ai in t.Value)
+                {
+                    var liquidItem = new
+                    {
+                        Title = ai.Title,
+                        Description = ai.Description,
+                        Url = ai.Url,
+                        Icon = ai.Icon
+                    };
+
+                    ail.Add(liquidItem);
+                }
+
+                liquidTagList.Add(t.Key, ail);
             }
-                        
-            liquidTagList.Add(t.Key, ail);
+
+            var result = template.Render(Hash.FromAnonymousObject(new { Tags = liquidTagList }));
+
+            File.WriteAllText(outputFile, result);
+
+            Console.WriteLine("Done");
         }
-
-        var result = template.Render(Hash.FromAnonymousObject(new { Tags = liquidTagList }));
-        
-        File.WriteAllText(outputFile, result);
-
-        Console.WriteLine("Done");
     }
 }
-
-
-
